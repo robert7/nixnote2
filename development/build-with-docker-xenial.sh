5672c50e769e4710b85ba4b3ac24deb5d84e100e
Robert Spiegel <1135218+robert7@users.noreply.github.com>
2018-08-25 14:55:24 +0200

    Improving docker build

--- development/build-with-docker-xenial.sh
+++ development/build-with-docker-xenial.sh
@@ -24,11 +24,20 @@ fi
 cd $PROJECTDIR
 # create "builder" image
 docker build -t nixnote2/xenial -f ./development/docker/Dockerfile.ubuntu_xenial ./development/docker
+
 # delete appdir content
-rm -rf appdir; mkdir appdir
+rm -rf appdir
+mkdir appdir
+
+if [ -d docker-build-debug ]; then
+  mkdir docker-build-debug
+fi
 
 # start container (note: each call creates new container)
-docker run -v $PROJECTDIR/appdir:/opt/nixnote2/appdir -it nixnote2/xenial \
+time docker run \
+   -v $PROJECTDIR/appdir:/opt/nixnote2/appdir \
+   -v $PROJECTDIR/docker-build-debug:/opt/nixnote2/qmake-build-debug \
+   -it nixnote2/xenial \
    /bin/bash -c "cd nixnote2 && git fetch && git checkout $PROJECTBRANCH && git pull && ./development/build-with-qmake.sh /usr debug && ./development/create-AppImage.sh && mv *.AppImage appdir && chmod -R a+rwx appdir"
 
 ls appdir/*.AppImage
