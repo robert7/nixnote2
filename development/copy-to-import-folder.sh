3a03c96a9f1c750a33d8c6afafbf9b10b7c9c153
Robert Spiegel <1135218+robert7@users.noreply.github.com>
2018-09-01 10:26:19 +0200

    Added helper script for "Import Folders"

---
+++ development/copy-to-import-folder.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# helper script for Import Folders
+# see https://github.com/robert7/nixnote2/issues/14
+
+
+if [ ! -f "$1" ]; then echo 1st param needs to be existing file!; exit 1; fi
+if [ ! -d "$2" ]; then echo 2nd param needs to be existing directory!; exit 1; fi
+
+
+# copy file with ".tmp" file extension to targed directory
+cp $1 $2/$1.tmp
+
+# rename file to original name
+mv $2/$1.tmp $2/$1
