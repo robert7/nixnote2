eb8e39751d3305fa9ba0a3ecd2cec7172e3a71c3
Robert Spiegel <1135218+robert7@users.noreply.github.com>
2018-09-29 18:35:08 +0200

    Fixed spell check include file

--- src/plugins/hunspell/hunspellinterface.h
+++ src/plugins/hunspell/hunspellinterface.h
@@ -25,7 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #define HUNSPELLINTERFACE_H
 
 #include <QtPlugin>
-#include "src/utilities/spellchecker.h"
+#include "../../utilities/spellchecker.h"
 
 
 class HunspellInterface
