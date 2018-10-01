5d157782f925659894a111984dee165ae06ca352
Robert Spiegel <1135218+robert7@users.noreply.github.com>
2018-10-01 13:35:42 +0200

    Fixed problem with spell check button always hidden (in toolbar)

--- src/gui/browserWidgets/editorbuttonbar.cpp
+++ src/gui/browserWidgets/editorbuttonbar.cpp
@@ -575,7 +575,7 @@ void EditorButtonBar::getButtonbarState() {
     highlightColorAction->setVisible(global.settings->value("highlightButtonVisible", true).toBool());
     highlightVisible->setChecked(highlightColorAction->isVisible());
 
-    spellCheckButtonAction->setVisible(global.settings->value("spelLCheckButtonVisible", false).toBool());
+    spellCheckButtonAction->setVisible(global.settings->value("spellCheckButtonVisible", true).toBool());
     spellCheckButtonVisible->setChecked(spellCheckButtonAction->isVisible());
 
     htmlEntitiesButtonAction->setVisible(global.settings->value("htmlEntitiesButtonVisible", false).toBool());
