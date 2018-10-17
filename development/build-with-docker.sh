2d647debd944a74d98027a458c0a7eec23945672
Robert Spiegel <1135218+robert7@users.noreply.github.com>
2018-10-17 13:29:12 +0200

    Experimental docker config files for other qt versions

--- development/build-with-docker.sh
+++ development/build-with-docker.sh
@@ -2,6 +2,9 @@
 PROJECTBRANCH=${1}
 PROJECTDIR=`pwd`
 
+DOCKERTAG=nixnote2/xenial
+DOCKERFILE=./development/docker/Dockerfile.ubuntu_xenial
+
 if [ ! -f src/main.cpp ]; then
   echo "You seem to be in wrong directory. script MUST be run from the project directory."
   exit 1
@@ -11,10 +14,9 @@ if [ -z "${PROJECTBRANCH}" ]; then
     PROJECTBRANCH=master
 fi
 
-
 cd $PROJECTDIR
 # create "builder" image
-docker build -t nixnote2/xenial -f ./development/docker/Dockerfile.ubuntu_xenial ./development/docker
+docker build -t ${DOCKERTAG} -f ${DOCKERFILE} ./development/docker
 
 if [ ! -d appdir ] ; then
   mkdir appdir
@@ -34,21 +36,23 @@ fi
 
 
 # to try manually:
-#
-# docker run --rm  -it nixnote2/xenial /bin/bash
-#  then
-#    PROJECTBRANCH=feature/rc1
-#    BUILD_TYPE=release
-#    ...copy command..
+#   DOCKERTAG=..
+#   docker run --rm  -it ${DOCKERTAG} /bin/bash
+#    then
+#      PROJECTBRANCH=feature/rc1
+#      BUILD_TYPE=release
+#      ...copy command from bellow & paste..
 # --------------------
+
+# for beineri PPA add: source /opt/qt*/bin/qt*-env.sh
 
 time docker run \
    --rm \
    -v $PROJECTDIR/appdir:/opt/nixnote2/appdir \
    -v $PROJECTDIR/docker-build-${BUILD_TYPE}:/opt/nixnote2/qmake-build-${BUILD_TYPE} \
    -v $PROJECTDIR/docker-build-${BUILD_TYPE}-t:/opt/nixnote2/qmake-build-${BUILD_TYPE}-t \
-   -it nixnote2/xenial \
-      /bin/bash -c "cd nixnote2 && git fetch && git checkout $PROJECTBRANCH && git pull && ./development/build-with-qmake.sh ${BUILD_TYPE} noclean /usr/lib/nixnote2/tidy && ./development/run-tests.sh ${BUILD_TYPE} noclean /usr/lib/nixnote2/tidy && ./development/create-AppImage.sh && mv *.AppImage appdir && chmod -R a+rwx appdir"
+   -it ${DOCKERTAG} \
+      /bin/bash -c "cd nixnote2 && git fetch && git checkout $PROJECTBRANCH && git pull  && ./development/build-with-qmake.sh ${BUILD_TYPE} noclean /usr/lib/nixnote2/tidy && ./development/run-tests.sh ${BUILD_TYPE} noclean /usr/lib/nixnote2/tidy && ./development/create-AppImage.sh && mv *.AppImage appdir && chmod -R a+rwx appdir"
 
 ls appdir/*.AppImage
 echo "If all got well then AppImage file in appdir is your binary"
