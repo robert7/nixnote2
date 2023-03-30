03837e6b619007e6f733635223d0ec4e6ee17cfa
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2023-03-30 17:38:21 +0300

    Issue #171: switch docker build to Ubuntu focal and using system browser for OAuth

--- development/build-with-docker.sh
+++ development/build-with-docker.sh
@@ -3,12 +3,8 @@ PROJECTBRANCH=${1}
 PROJECTDIR=`pwd`
 set -xe
 
-# note: all with DOCKERMODIFIER != "" is highly experimental and needs "someway" to include webkit binaries
-# so without it will fail
-#DOCKERMODIFIER=_qt562
-
-DOCKERTAG=nixnote2/xenial${DOCKERMODIFIER}
-DOCKERFILE=./development/docker/Dockerfile.ubuntu_xenial${DOCKERMODIFIER}
+DOCKERTAG=nixnote2/focal
+DOCKERFILE=./development/docker/Dockerfile.ubuntu_focal
 
 function error_exit {
     echo "$0: ***********error_exit***********"
@@ -33,12 +29,6 @@ cd $PROJECTDIR
 docker build -t ${DOCKERTAG} -f ${DOCKERFILE} ./development/docker
 
 # stop after creating the image (e.g. you want to do the build manually)
-if [ ! -z ${DOCKERMODIFIER} ] ; then
-  echo "Docker image ${DOCKERTAG} created.. "
-  echo "DOCKERMODIFIER set to $DOCKERMODIFIER .. you need to provide webkit manually.."
-  exit 1
-fi
-
 if [ ! -d appdir ] ; then
   mkdir appdir || error_exit "mkdir appdir"
 fi
@@ -53,33 +43,13 @@ if [ ! -d docker-build-${BUILD_TYPE} ]; then
 fi
 
 # start container (note: each call creates new container)
-
-
-
-# to try manually:
-#   DOCKERTAG=..
-#   docker run --rm  -it ${DOCKERTAG} /bin/bash
-#    then
-#      PROJECTBRANCH=feature/rc1
-#      BUILD_TYPE=release
-#      ...copy command from bellow & paste..
-# --------------------
-
-# **TEMPORARY** for beineri PPA recompile #################
-# PROJECTBRANCH=feature/rc1;BUILD_TYPE=release
-# source /opt/qt*/bin/qt*-env.sh
-# git fetch && git checkout $PROJECTBRANCH && git pull  && ./development/build-with-qmake.sh ${BUILD_TYPE} noclean /usr/lib/nixnote2/tidy
-# unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
-# ./development/create-AppImage.sh
-# mv *.AppImage appdir2 && chmod -R a+rwx appdir/*.AppImage
-
 time docker run \
    --rm \
    -v $PROJECTDIR/appdir:/opt/nixnote2/appdir \
    -v $PROJECTDIR/docker-build-${BUILD_TYPE}:/opt/nixnote2/qmake-build-${BUILD_TYPE} \
    -v $PROJECTDIR/docker-build-${BUILD_TYPE}-t:/opt/nixnote2/qmake-build-${BUILD_TYPE}-t \
    -it ${DOCKERTAG} \
-      /bin/bash -c "cd nixnote2 && git fetch && git checkout $PROJECTBRANCH && git pull  && ./development/build-with-qmake.sh ${BUILD_TYPE} noclean /usr/lib/nixnote2/tidy && ./development/run-tests.sh ${BUILD_TYPE} noclean /usr/lib/nixnote2/tidy && ./development/create-AppImage.sh && mv *.AppImage appdir && chmod -R a+rwx appdir/*.AppImage"
+      /bin/bash -c "cd nixnote2 && git fetch && git checkout $PROJECTBRANCH && git reset --hard origin/$PROJECTBRANCH && ./development/build-with-qmake.sh ${BUILD_TYPE} noclean /usr/lib/nixnote2/tidy browser && ./development/run-tests.sh ${BUILD_TYPE} noclean /usr/lib/nixnote2/tidy && ./development/create-AppImage-linuxdeploy.sh && mv *.AppImage appdir && chmod -R a+rwx appdir/*.AppImage"
 
 ls appdir/*.AppImage
 echo "If all got well then AppImage file in appdir is your binary"
